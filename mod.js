5984203cce68b0d4f6618d7327495b990755aab0
Jeason <me@jeasonstudio.cn>
2021-04-08 11:23:08 +0800

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,57 +1,26 @@
-function handleRequest(request) {
-  const { pathname } = new URL(request.url);
+// You need to import `h` factory function as Deno Deploy
+// uses it instead of `React.createElement`
+import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
+import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
 
-  if (pathname.startsWith("/html")) {
-    const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
-      </html>`;
-
-    return new Response(html, {
-      headers: {
-        // The interpretation of the body of the response by the client depends
-        // on the 'content-type' header.
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        "content-type": "text/html; charset=UTF-8",
-      },
-    });
-  }
-
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
-
-  return new Response(
-    `<body
-      align="center"
-      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-    >
-      <h1>Return JSON and/or HTML Example</h1>
-      <p>
-        <a href="/html">/html</a> - responds with HTML to the request.
-      </p>
-      <p>
-        <a href="/json">/json</a> - responds with JSON to the request.
-      </p>
-    </body>`,
-    {
-      headers: {
-        "content-type": "text/html; charset=UTF-8",
-      },
-    },
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Hello from JSX</title>
+      </head>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+    </html>
   );
 }
 
 addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
+  // renderToString generates html string from JSX components.
+  const response = new Response(renderToString(<App />), {
+    headers: { "content-type": "text/html; charset=uft-8" },
+  });
+
+  event.respondWith(response);
 });
